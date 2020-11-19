52eb275812d5fc874986928944eeff32a7e68dcc
MaximeNef <maximenef1@gmail.com>
2020-11-19 16:17:30 +0100

    option shuffle rajouté

--- kitchen.js
+++ kitchen.js
@@ -1,20 +1,62 @@
 const ul = document.querySelector('ul')
 const enfant = ul.childNodes;
 console.log(enfant)
-for(let list of enfant){
-    
-    if(list.nodeType === 1){
-    console.log(list.textContent)
-    
+
+
+//for (let list of enfant) 
+
+for (let i = 0; i < enfant.length; i++) {
+    const list = enfant[i];
+
+    if (list.nodeType === 1) {
+        console.log(list.textContent)
+
     }
-   if(list.textContent ==='Fast and Furious'){
-    
-    const first = ul.firstElementChild
-    list.parentElement.insertBefore(list, first )
-    list.className = 'important'
-   }
-   list.addEventListener('click' , function(){
-       alert(list.textContent)
-      
-   })
+
+    if (list.textContent === 'Fast and Furious') {
+        const first = ul.firstElementChild
+        list.parentElement.insertBefore(list, first)
+        list.className = 'important'
+    }
+
+
+    list.addEventListener('click', function () {
+
+        if (list.textContent === 'Fast and Furious') {
+            alert(list.textContent + " :The most important franchise ever, the story of DOM(inic) Toretto's family. It's not about car, it's about family.")
+        } else {
+            alert(list.textContent)
+        }
+    })
+
+    for (j = i + 1; j < enfant.length; j++) {
+        const newList = enfant[j];
+        if (list.isEqualNode(newList)) {
+            ul.removeChild(newList)
+        }
+    }
 }
+
+
+function shuffle() {
+    const oldUl = document.querySelector('ul')
+    const newUl = document.createElement('ul')
+
+    newUl.appendChild(oldUl.children[0])
+
+    for (let i = 0; oldUl.children.length > 0; i++) {
+        const oldLi = oldUl.children
+        const randomIndex = Math.floor(oldLi.length * Math.random())
+        newUl.appendChild(oldLi[randomIndex])
+    }
+
+    document.body.removeChild(oldUl)
+    document.body.appendChild(newUl)
+
+}
+
+document.body.addEventListener('keyup', function (e) {
+    if (e.key == 'r') {
+        shuffle()
+    }
+})
